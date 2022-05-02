e65d238a530ed818d7d7ae0ff06ee89807eb884b
Max Kasapovich <crazywarm@ukr.net>
2022-05-02 23:00:56 +0300

    111

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,15 +6,14 @@ public class Main {
 
     public static void main(String[] args) {
         for (int i = 0; i <GlobalRating.data[i].category.length(); i++) {
-            System.out.println(GlobalRating.data[i].category[i]);
-            for (int j = 0; j <GlobalRating.data[i].statistic[j]; j++) {
-                System.out.println(GlobalRating.data[i].statistic[j]);
+            System.out.println(GlobalRating.data[i].category);
+            for (int j = 0; j <GlobalRating.data[i].statistic.length; j++) {
+                if (j==(GlobalRating.data[i].statistic.length) - 1) {
+                    System.out.print(GlobalRating.data[i].statistic[j]);
+                } else {
+                    System.out.print(GlobalRating.data[i].statistic[j] + " ");
+                }
             }
         }
-
-        System.out.println(Arrays.toString(GlobalRating.data[0].statistic));
-        System.out.println(Arrays.toString(GlobalRating.data[1].statistic));
-        System.out.println(Arrays.toString(GlobalRating.data[2].statistic));
-        System.out.println(Arrays.toString(GlobalRating.data[3].statistic));
     }
 }
