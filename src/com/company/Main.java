f45418d7b3469e488cd8e3b9cc3b8c2c90b247a6
Max Kasapovich <crazywarm@ukr.net>
2022-05-02 23:11:29 +0300

    666

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,13 +5,13 @@ import java.util.Arrays;
 public class Main {
 
     public static void main(String[] args) {
-        for (int i = 0; i <GlobalRating.data[i].category.length(); i++) {
-            System.out.println(GlobalRating.data[i].category);
+        for (int i = 0; i <GlobalRating.data.length; i++) {
+            System.out.print("\n" + GlobalRating.data[i].category);
             for (int j = 0; j <GlobalRating.data[i].statistic.length; j++) {
                 if (j==(GlobalRating.data[i].statistic.length - 1)) {
                     System.out.print(GlobalRating.data[i].statistic[j]);
                 } else {
-                    System.out.print(GlobalRating.data[i].statistic[j] + " ");
+                    System.out.print(" " + GlobalRating.data[i].statistic[j]);
                 }
             }
         }
